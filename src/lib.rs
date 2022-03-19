8681ad9f551e40fb2f7c18b1c2b2ce2e849658e0
Owen Shepherd <owen@owen.cafe>
2022-03-19 15:33:48 +0000

    Extract out create_homogeneous_future, add unordered version

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,100 @@
-pub mod util {
-  use futures::future::Either;
+pub mod internal {
+    use futures::future::Either;
 
-  pub fn left_homogenous<A>(a: A) -> Either<A, A> {
-    Either::Left(a)
-  }
+    pub fn left_homogenous<A>(a: A) -> Either<A, A> {
+        Either::Left(a)
+    }
 }
 
 #[macro_export]
-macro_rules! futures_to_stream {
-  (impl) => {
+macro_rules! create_homogeneous_future {
+  () => {
     {
       use std::option::*;
       use futures::future::Either;
       Either::Right(empty())
     }
   };
-  (impl $x:expr) => {
+  ($x:expr) => {
     {
-      Some($crate::util::left_homogenous($x)).into_iter()
+      Some($crate::internal::left_homogenous($x)).into_iter()
     }
   };
-  (impl $x:expr, $($tail:expr),*) => {
+  ($x:expr, $($tail:expr),*) => {
     {
       use futures::future::Either;
       use std::iter::Iterator;
 
       Some(Either::Left($x)).into_iter().chain(
-        futures_to_stream!(impl $($tail),*).map(Either::Right)
+        $crate::create_homogeneous_future!($($tail),*).map(Either::Right)
       )
     }
   };
+}
+
+#[macro_export]
+macro_rules! futures_to_ordered_stream {
   ($($tail:tt)*) => {
     {
       use futures::stream::FuturesOrdered;
 
-      let fs = futures_to_stream!(impl $($tail)*);
-      FuturesOrdered::from_iter(fs)
+      let futs = $crate::create_homogeneous_future!($($tail)*);
+      FuturesOrdered::from_iter(futs)
+    }
+  }
+}
+
+#[macro_export]
+macro_rules! futures_to_unordered_stream {
+  ($($tail:tt)*) => {
+    {
+      use futures::stream::FuturesUnordered;
+
+      let futs = $crate::create_homogeneous_future!($($tail)*);
+      FuturesUnordered::from_iter(futs)
     }
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use futures::stream::{Stream, StreamExt};
+    use futures::stream::{Stream, StreamExt};
+    use tokio::time::{sleep, Duration};
 
-  async fn test1() -> () { () }
-  async fn test2() -> () { () }
-  async fn test3() -> () { () }
-  async fn test4() -> () { () }
-  
-  fn futs() -> impl Stream<Item = ()> {
-    futures_to_stream!(
-      test1(),
-      test2(),
-      test3(),
-      test4()
-    )
-  }
+    async fn test1() -> u8 {
+        sleep(Duration::from_millis(400)).await;
+        1
+    }
+    async fn test2() -> u8 {
+        sleep(Duration::from_millis(300)).await;
+        2
+    }
+    async fn test3() -> u8 {
+        sleep(Duration::from_millis(200)).await;
+        3
+    }
+    async fn test4() -> u8 {
+        sleep(Duration::from_millis(100)).await;
+        4
+    }
 
-  #[tokio::test]
-  async fn test() {
-    let res: Vec<()> = futs().collect().await;
-    assert_eq!(vec![(), (), (), ()], res);
-  }
+    fn futs_ordered() -> impl Stream<Item = u8> {
+        futures_to_ordered_stream!(test1(), test2(), test3(), test4())
+    }
+
+    fn futs_unordered() -> impl Stream<Item = u8> {
+        futures_to_unordered_stream!(test1(), test2(), test3(), test4())
+    }
+
+    #[tokio::test]
+    async fn test_ordered() {
+        let res: Vec<u8> = futs_ordered().collect().await;
+        assert_eq!(vec![1, 2, 3, 4], res);
+    }
+
+    #[tokio::test]
+    async fn test_unordered() {
+        let res: Vec<u8> = futs_unordered().collect().await;
+        assert_eq!(vec![4, 3, 2, 1], res);
+    }
 }
